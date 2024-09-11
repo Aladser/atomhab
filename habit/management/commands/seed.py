19be935c937d5b48d9b02f0f0d35a9170ad6bde5
Aladser <Aladser@users.noreply.github.com>
2024-09-11 23:26:25 +0900

    обновлено сидирования таблиц

--- habit/management/commands/seed.py
+++ habit/management/commands/seed.py
@@ -5,17 +5,6 @@ from authen_drf.management.commands.createusers import user_1_email, user_2_emai
 from habit.models import *
 from libs.seeding import Seeding
 
-dateperiod_param_obj_list = [
-    {'name':'полчаса', 'interval': 60 * 30},
-    {'name':'1 час', 'interval': 60 * 60},
-    {'name':'3 часа', 'interval': 60 * 60 * 3},
-    {'name': '6 часов', 'interval': 60 * 60 * 6},
-    {'name': '12 часов', 'interval': 60 * 60 * 12},
-    {'name': '1 день', 'interval': 60 * 60 * 24},
-    {'name': '3 дня', 'interval': 60 * 60 * 24 * 3},
-    {'name': '1 неделя', 'interval': 60 * 60 * 24 * 7},
-]
-
 place_param_obj_list = [
     {'name': 'Дом'},
     {'name': 'Работа'},
@@ -28,12 +17,12 @@ action_param_obj_list = [
     {'name': 'бегать'},
     {'name': 'учиться'},
     {'name': 'отжиматься'},
-    {'name': 'убираться'},
-    {'name': 'кормить кота'},
-    {'name': 'убираться'},
     {'name': 'петь'},
     {'name': 'играть'},
     {'name': 'танцевать'},
+    {'name': 'убираться'},
+    {'name': 'кормить кота'},
+    {'name': 'убираться'},
 ]
 
 reward_param_obj_list = [
@@ -42,6 +31,16 @@ reward_param_obj_list = [
     {'name': 'выпить воду'},
     {'name': 'играть с котом'},
     {'name': 'погулять на улице'},
+]
+
+dateperiod_param_obj_list = [
+    {'name':'полчаса', 'interval': 60 * 30},
+    {'name':'1 час', 'interval': 60 * 60},
+    {'name': '6 часов', 'interval': 60 * 60 * 6},
+    {'name': '12 часов', 'interval': 60 * 60 * 12},
+    {'name': '1 день', 'interval': 60 * 60 * 24},
+    {'name': '3 дня', 'interval': 60 * 60 * 24 * 3},
+    {'name': '1 неделя', 'interval': 60 * 60 * 24 * 7},
 ]
 
 class Command(BaseCommand):
@@ -68,12 +67,13 @@ class Command(BaseCommand):
         reward_3 =get_object_or_404(Reward, name=reward_param_obj_list[2]['name'])
 
         hour_period = get_object_or_404(DatePeriod, name=dateperiod_param_obj_list[1]['name'])
-        day_period = get_object_or_404(DatePeriod, name=dateperiod_param_obj_list[5]['name'])
-        week_period = get_object_or_404(DatePeriod, name=dateperiod_param_obj_list[7]['name'])
+        day_period = get_object_or_404(DatePeriod, name=dateperiod_param_obj_list[4]['name'])
+        week_period = get_object_or_404(DatePeriod, name=dateperiod_param_obj_list[6]['name'])
 
         habit_param_obj_list = [
             {'user':user_1, 'place':place_1, 'action':action_1, 'interval': hour_period, 'reward':reward_1},
             {'user': user_2, 'place': place_2, 'action': action_2, 'interval': day_period, 'reward': reward_2},
             {'user': user_3, 'place': place_3, 'action': action_3, 'interval': week_period, 'reward': reward_3},
         ]
+
         Seeding.seed_table(Habit, habit_param_obj_list)
