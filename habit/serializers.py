4632e843a6b109e292fed5197a22b9e737d6cb78
Aladser <Aladser@users.noreply.github.com>
2024-09-13 06:58:03 +0900

    добавлен Action view, serializer

--- habit/serializers.py
+++ habit/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from habit.models import Location
+from habit.models import Location, Action
 
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
         fields = '__all__'
+
+class ActionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Action
+        fields = '__all__'
+
--- habit/views.py
+++ habit/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 
-from habit.models import Location
-from habit.serializers import LocationSerializer
+from habit.models import Location, Action
+from habit.serializers import LocationSerializer, ActionSerializer
 
 
 # LOCATION LIST
@@ -9,3 +9,7 @@ class LocationListAPIView(generics.ListAPIView):
     serializer_class = LocationSerializer
     queryset = Location.objects.all()
 
+# ACTION LIST
+class ActionListAPIView(generics.ListAPIView):
+    serializer_class = ActionSerializer
+    queryset = Action.objects.all()
