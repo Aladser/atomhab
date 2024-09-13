74f08890eb54a4b9c7c8f67a0f20b32b57e459f1
Aladser <Aladser@users.noreply.github.com>
2024-09-13 09:48:50 +0900

    добавлены контроллеры Location, Action, Reward

--- habit/urls.py
+++ habit/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
 
 from habit.apps import HabitConfig
-from habit.views import LocationListAPIView, ActionListAPIView, RewardListAPIView
+from habit.views import LocationListAPIView, ActionListAPIView, RewardListAPIView, LocationCreateAPIView, \
+    LocationDestroyAPIView, ActionCreateAPIView, ActionDestroyAPIView, RewardCreateAPIView, RewardDestroyAPIView
 
 app_name = HabitConfig.name
 
 urlpatterns = [
     path('location/', LocationListAPIView.as_view(), name='location-list'),
+    path('location/create/', LocationCreateAPIView.as_view(), name='location-create'),
+    path('location/<int:pk>/delete', LocationDestroyAPIView.as_view(), name='location-delete'),
+
     path('action/', ActionListAPIView.as_view(), name='action-list'),
+    path('action/create/', ActionCreateAPIView.as_view(), name='action-create'),
+    path('action/<int:pk>/delete', ActionDestroyAPIView.as_view(), name='action-delete'),
+
     path('reward/', RewardListAPIView.as_view(), name='reward-list'),
+    path('reward/create/', RewardCreateAPIView.as_view(), name='reward-create'),
+    path('reward/<int:pk>/delete', RewardDestroyAPIView.as_view(), name='reward-delete'),
 ]
--- habit/views.py
+++ habit/views.py
@@ -9,12 +9,41 @@ class LocationListAPIView(generics.ListAPIView):
     serializer_class = LocationSerializer
     queryset = Location.objects.all()
 
+# LOCATION CREATE
+class LocationCreateAPIView(generics.CreateAPIView):
+    serializer_class = LocationSerializer
+
+# LOCATION DESTROY
+class LocationDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = LocationSerializer
+    queryset = Location.objects.all()
+
+
 # ACTION LIST
 class ActionListAPIView(generics.ListAPIView):
     serializer_class = ActionSerializer
     queryset = Action.objects.all()
 
+# ACTION CREATE
+class ActionCreateAPIView(generics.CreateAPIView):
+    serializer_class = ActionSerializer
+
+# ACTION DESTROY
+class ActionDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = ActionSerializer
+    queryset = Action.objects.all()
+
+
 # REWARD LIST
 class RewardListAPIView(generics.ListAPIView):
+    serializer_class = RewardSerializer
+    queryset = Reward.objects.all()
+
+# REWARD CREATE
+class RewardCreateAPIView(generics.CreateAPIView):
+    serializer_class = RewardSerializer
+
+# REWARD DESTROY
+class RewardDestroyAPIView(generics.DestroyAPIView):
     serializer_class = RewardSerializer
     queryset = Reward.objects.all()
