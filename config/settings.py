bcef65313c26cdd23d8748d3ac62c4cb83de8757
Aladser <Aladser@users.noreply.github.com>
2024-09-15 07:00:17 +0900

    добавлена документация

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'django_celery_beat',
+    'drf_yasg',
     'corsheaders',
 
     'authen_drf',
--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,30 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
 
 from config.settings import MEDIA_ROOT, MEDIA_URL
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Atomic habits API",
+      default_version='v1',
+      description="Полезные привычки",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('authen_drf.urls', namespace='user')),
     path('', include('habit.urls')),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
